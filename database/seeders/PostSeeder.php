6aae46f20830ad69b8421866469e157503bf0f3c
kotorinu <koto.tama.yellow@gmail.com>
2023-10-01 20:12:28 +0900

    テーブル作れた

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('posts')->insert([
+                'artist_name' => 'アーティスト名',
+                'venue' => '会場名',
+                'seat' => '座席位置',
+                'stage_distance' => '近さ',
+                'others' => 'その他',
+                'fan_service' => 'ファンサ',
+                'falling_objects' => '落下物',
+                'visible' => '肉眼',
+                'comment' => '感想',
+                'created_at' => new DateTime(),
+                'updated_at' => new DateTime(),
+         ]);
+    }
+}
